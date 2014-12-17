b0ff8395747eae4a3055a3dfd30e014de5fcc0de
Takahisa Kudo <t-kudo@iij.ad.jp>
2014-12-17 16:53:06 +0900

    [change] fix node.js version. defaut(0.10.26) to 0.10.32. for el.

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,3 +3,6 @@ default["daab"]["user"] = "daab"
 default["daab"]["group"] = "daab"
 
 default["daab"]["daabsdk_zipfile_url"] = "https://direct4b.com/ja/bot/dev/samples/starter/starter-0.1.0.zip"
+
+default['nodejs']['version'] = '0.10.32'
+default['nodejs']['source']['checksum'] = "c2120d0e3d2d191654cb11dbc0a33a7216d53732173317681da9502be0030f10"
